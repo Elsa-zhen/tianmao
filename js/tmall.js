5cf0c9f6aef1c4409047f735284b8c72531af6ed
Elsa-zhen <984802925@qq.com>
2017-10-23 09:18:27 +0800

    1

--- js/tmall.js
+++ js/tmall.js
@@ -67,7 +67,7 @@ let obj;
 
 	window.addEventListener("scroll",function(){
 	    let st=obj.scrollTop;
-	    if(st > 778){
+	    if(st >790){
             smbox.style.cssText="width:35px;height:370px;"
         }else{
             smbox.style.cssText="width:0;height:0;"
@@ -75,35 +75,100 @@ let obj;
 
 
         for(let i=0;i<nrbox.length;i++){
-	        if(st >= nrbox[i].offsetTop - 50){
+	        if(st >= nrbox[i].offsetTop - 500){
                 for(let j=0;j<list.length;j++){
                     list[j].classList.remove("active");
                 }
                 list[i].classList.add("active");
             }
         }
-    })
+    });
     list.forEach(function(val,index){
         val.onclick=function(){
             let ot=nrbox[index].offsetTop;
-            animate(obj,{scrollTop:ot - 60})
+            animate(obj,{scrollTop:ot -10})
+        }
+    });
+
+    //返回顶部
+    let totop=document.querySelector(".leftbar-totop");
+    window.addEventListener("scroll",function(){
+        if(obj.scrollTop>1200){
+            totop.onclick=function(){
+                animate(obj,{scrollTop:0})
+            }
+        }
+    })
+
+
+}
+
+//返回顶部
+{
+    let totop=document.querySelector(".cebox2-totop");
+    console.log(totop);
+
+        window.addEventListener("scroll",function(){
+            let st=obj.scrollTop;
+            if(st > 500){
+                totop.style.opacity="1";
+            }
+            else{
+                totop.style.opacity="0";
+            }
+        });
+        totop.onclick=function(){
+            animate(obj,{scrollTop:0})
+        }
+}
+
+//按需加载
+{
+
+
+    // console.log(imgs);
+
+    window.addEventListener("scroll",function(){
+        let st=obj.scrollTop;
+        let boxs=document.querySelectorAll(".nrbox");
+        for(let i=0;i<boxs.length;i++){
+            let imgs=document.querySelectorAll(".beauty-middle-box-img img");
+            if(st >=boxs[i].offsetTop - window.innerHeight){
+                for(let j=0;j<imgs.length;j++){
+                    imgs[j].src=imgs[j].getAttribute('data-src');
+                }
+            }
         }
     })
 }
 
-//返回顶部
-/*{
-    let totop=document.querySelectorAll(".totop");
-    console.log(totop);
-    window.addEventListener("scroll",function(){
-        let st=obj.scrollTop;
-        if(st > 778){
-            totop.style.opacity="1";
-        }else{
-            totop.style.opacity="0";
-        }
-    })
-    totop.onclick=function(){
-        animate(obj,{scrollTop:0});
+//轮播左侧菜单栏
+{
+    let banner=document.querySelector("#banner");
+    let leftObj=document.querySelector(".banner-img-left");
+    let listObj=document.querySelector(".banner-img-left-left");
+    let lieObj=document.querySelectorAll(".banner-img-left h6");
+    console.log(banner,leftObj,listObj,lieObj);
+
+
+    leftObj.onmouseover=function(){
+        listObj.style.display="block";
     }
-}*/
+    leftObj.onmouseout=function(){
+        listObj.style.display="none";
+    }
+    listObj.onmouseover=function(){
+        listObj.style.display="block";
+    }
+    listObj.onmouseout=function(){
+        listObj.style.display="none";
+    }
+    banner.onmouseleave=function(){
+        listObj.style.display="none";
+    }
+
+
+
+
+
+}
